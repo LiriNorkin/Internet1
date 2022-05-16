ff011d2c5fc1694517f6291ce9e081023e9e36c9
Tamir Sadovsky <tamirsadovsky@gmail.com>
2022-05-16 19:28:57 +0300

    Updated birthday & birth country. Added comments. ontology uploaded

--- geo_qa.py
+++ geo_qa.py
@@ -8,6 +8,7 @@ import re
 import traceback
 import lxml.html
 import rdflib
+from dateutil.parser import parse
 
 ### PART 1: Crawler & Extraction from infobox to ontology
 
@@ -18,29 +19,43 @@ visited = set()
 g = rdflib.Graph()
 url_queue = queue.Queue() # queue of urls: (Type, URL), Type = Country / President / Prime_Minister
 
-### added /wiki to prefix
+wiki_pre = "/wiki/"
 prefix = "http://en.wikipedia.org"
 ontology_prefix = "http://example.org/"
 url_source = "https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)"
 
-data_labels = ["president", "prime_minister", "population", "area", "government", "capital", "Country"]
+data_labels = ["president", "prime_minister", "population", "area", "government", "capital", "Country", "where_born", "when_born"]
 g = rdflib.Graph()
 count_based_on = 0
 
 def extract_country_from_url(url):
-    return url.split("/")[-1].replace("_"," ")
+    # This function gets a wikipedia link of a country and returns the name of the country
+    return url.split("/")[-1].replace("_", " ")
 
 def data_spaces_to_underlines(question):
+    # This function replaces spaces of given string with underlines
     return question.replace(" ", "_")
 
 def data_hyphens_to_underlines(question):
+    # This function replaces hyphens of given string with underlines
     return question.replace("-", "_")
 def remove_hyphens(question):
+    # This function removes hyphens of given string
     return question.replace("-", "")
 def remove_underlines(question):
+    # This function removes underlines of given string
     return question.replace("_", "")
 
 def add_to_ontology(entity, description, result):
+    """
+    This function gets a triple of:
+    @param entity = country / prime minister / president
+    @param description = from data_labels (list)
+    @param result data = population, capital, etc
+
+    Result: adds the data to the ontology
+    """
+
     if len(entity) > 0 and len(description) > 0 and len(result) > 0:
         entity = f"{ontology_prefix}{entity}"
         description = f"{ontology_prefix}{description}"
@@ -49,6 +64,12 @@ def add_to_ontology(entity, description, result):
         g.add((rdflib.URIRef(entity), rdflib.URIRef(description), rdflib.URIRef(result)))
 
 def from_source_url_to_queue():
+    """
+    This function initialize the url_queue.
+    It enters a tuple of: <"Country"> , <Country URL> to queue.
+    special array handles special cases of 3 countries that have symbols in their wikipedia link page.
+    In addition it initialize countries list from the given page in the task.
+    """
     r = requests.get(url_source)
     doc = lxml.html.fromstring(r.content)
     special = [163, 196, 203]
@@ -64,29 +85,35 @@ def from_source_url_to_queue():
                 t = doc.xpath('//*[@id="mw-content-text"]/div[1]/table/tbody//tr/td[1]//a[@title]/@title')
                 t = t[special[count]]
                 count = count + 1
-                wiki = prefix + "/wiki/"
+                wiki = prefix + wiki_pre
                 wiki = data_spaces_to_underlines(wiki)
                 #print(t)
                 url_queue.put((data_labels[6], f"{wiki}{t}"))
             else:
                 url_queue.put((data_labels[6], f"{prefix}{t}"))
 
-            countries.append(t[6:len(t)])
-            countries.append(remove_underlines(t[6:len(t)]))
+            if t[6:len(t)] != "o" and t[6:len(t)] != "n":
+                countries.append(t[6:len(t)])
+                countries.append(remove_underlines(t[6:len(t)]))
     #print(inte)
 
-def add_population(country, url):
-    r = requests.get(url)
-    doc = lxml.html.fromstring(r.content)
+def add_population(country, doc):
+    """
+    This function extract the population size of country with XPATH query.
+    @param country = given country
+    @param doc = country page
+
+    Result: adds the data to the ontology
+    """
     population = doc.xpath('//table[contains(@class,"infobox")]/tbody//tr[contains(.//text(),"Population")]/following-sibling::tr/td//text()')
-    russia = 'http://en.wikipedia.org/wiki/Russia' # xpath is exception
-    if url == russia:
-        print('here')
+    russia = 'http://en.wikipedia.org/wiki/Russia' #xpath is exception
+    if country == "Russia":
+        #print('here')
         population = doc.xpath('//table[contains(@class,"infobox")]/tbody//tr[contains(.//text(),"Population")]/following-sibling::tr/td/div/ul/li/text()')
         #print(population)
     if population:
         population = population[0].split("(")[0]
-        population = str(population).replace(".", ",").replace(" ","")
+        population = str(population).replace(".", ",").replace(" ", "")
         #print(population)
         add_to_ontology(country, data_labels[2], str(population))
 
@@ -95,9 +122,14 @@ def add_population(country, url):
         # Russia = //a[text()="Population"]/following::tr[1]/td/div/ul/li/text()
         #print(str(population).replace(",", "_"))
 
-def add_government(country, url):
-    r = requests.get(url)
-    doc = lxml.html.fromstring(r.content)
+def add_government(country, doc):
+    """"
+    This function extract the form government of country with XPATH query.
+    @param country = given country
+    @param doc = country page
+
+    Result: adds the data to the ontology
+    """
     government = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Government"]//a/@href')
     government = government[1:]
     val_to_remove = []
@@ -117,80 +149,109 @@ def add_government(country, url):
         add_to_ontology(country, data_labels[4], str(government))
 
 def add_birth_location(person, url):
-    #print(person)
+    """"
+    This function extract the birth location of prime minister / president with XPATH query.
+    @param person = prime minister / president
+    @param url = prime minister / president wikipedia page
+
+    Result: adds the data to the ontology
+    """
     r = requests.get(url)
     doc = lxml.html.fromstring(r.content)
-    location_table = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Born"]/td//text()')
+    location_list = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Born"]/td//text()')
     location = ""
-    for i in location_table:
-        check = i.replace(" ", "_")
+    for i in location_list:
+        check = data_spaces_to_underlines(i)
         if check in countries:
             location = i
-            #print(location)
             break
         else:
             # replace chars that can be in string with spaces
             check = i.replace(" ", "").replace(",", "").replace(".", "").replace(")", "").replace("(", "")
-            #print(check)
             if check in countries:
                 location = check
                 break
     if location:
         location = data_spaces_to_underlines(location)
-        #print(location)
-        add_to_ontology(person, "where_born", location)
+        add_to_ontology(person, data_labels[7], location)
+    else:
+        for country in countries:
+            for location_check in location_list:
+                if country in location_check:
+                    location = country
+                    break
+        if location:
+            location = data_spaces_to_underlines(location)
+            add_to_ontology(person, data_labels[7], location)
+
 
 def add_birthday(person, url):
-    #### שווה בדיקה אם יש תאריכים שהתפספסו ##
+    """"
+    This function extract the birthday of prime minister / president with XPATH query.
+    @param person = prime minister / president
+    @param url = prime minister / president wikipedia page
+
+    Result: adds the data to the ontology
+    """
     #print(person)
-    test = ""
     r = requests.get(url)
     doc = lxml.html.fromstring(r.content)
     birthday = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Born"]//span[@class="bday"]//text()')
     #print(birthday)
-    text_date = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Born"]//td//text()')
-    #print(text_date)
-    if text_date:
-        for i in text_date:
-            #print(i)
-            if "19" in i:
-                test = i
-                break
-        if test and len(test) == 10:
-            test = data_hyphens_to_underlines(test)
-            #print(test)
-            add_to_ontology(person, "when_born", test)
+    if birthday:
+        birthday = data_hyphens_to_underlines(birthday[0])
+        add_to_ontology(person, data_labels[8], birthday)
 
+def add_area(country, doc):
+    """"
+    This function extract the area size of country with XPATH query.
+    @param country = given country
+    @param doc = country page
 
-def add_area(country, url):
-    r = requests.get(url)
-    doc = lxml.html.fromstring(r.content)
+    Result: adds the data to the ontology
+    """
     area = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr//td[text()[contains(.,"km")]]//text()')
     if len(area) > 0:
         area = str(area[0].split()[0])
         #print(area)
         add_to_ontology(country, data_labels[3], area)
 
-def add_capital(country, url):
-    r = requests.get(url)
-    doc = lxml.html.fromstring(r.content)
+def add_capital(country, doc):
+    """"
+    This function extract the capital city of country with XPATH query.
+    @param country = given country
+    @param doc = country page
+
+    Result: adds the data to the ontology
+    """
     capital = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Capital"]//@title')
     if capital:
         capital = data_spaces_to_underlines(capital[0])
-        add_to_ontology(country, "capital", capital)
+        add_to_ontology(country, data_labels[5], capital)
         #print(capital)
     else:
         if country == "Channel_Islands":
             capital = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Capital and largest settlement"]//@title')
             capital = data_spaces_to_underlines(capital[0])
-            add_to_ontology(country, "capital", capital)
+            add_to_ontology(country, data_labels[5], capital)
     ### האם צריך להתייחס למקרים כמו Washington,_D.C.  ###
 
 def add_president_or_prime_minister(country, person, url_person, role):
+    """"
+    This function extract the form government of country with XPATH query.
+    @param country = given country
+    @param person = prime minister / president name
+    @param url_person = prime minister / president wikipedia page
+    @param role = role of entity from data_labels (prime minister / president)
+
+    Result: adds the data to the ontology
+    """
+    #print(person)
+    #print(url_person)
     if url_person:
         url_person = url_person[0]
-    else:
-        url_person = "error"
+    #else:
+    #    url_person = "error"
     if person:
         person = person[0]
         person = data_spaces_to_underlines(person)
@@ -202,7 +263,16 @@ def add_president_or_prime_minister(country, person, url_person, role):
         add_to_ontology(person, role, country)
 
 def get_from_url(job):
-    dict = {}
+    """"
+    Main function.
+    The function manages every country url it gets from the initialize_crawl function.
+    The function sends each request to a small function which extracts all the data
+    it needs from country like: area,capital,population etc.
+
+    @param job = tuple of : <"Country"> , <Country URL>.
+
+    Result: manages the build of the ontology
+    """
     url = job[1]
     #print(url)
     print(url)
@@ -210,34 +280,41 @@ def get_from_url(job):
     print(country)
     r = requests.get(url)
     doc = lxml.html.fromstring(r.content)
+
     #### אפשר לשלוח לכל פונקציות הבת שלנו את r ו - doc ###
     president = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="President"]/td//text()')
     url_president = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="President"]/td//a/@href')
     prime_minister = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Prime Minister"]/td//text()')
     url_prime_minister = doc.xpath('//table[contains(@class, "infobox")]/tbody/tr[th//text()="Prime Minister"]/td//a/@href')
-    add_capital(country, url)
-    add_area(country, url)
-    add_government(country, url)
-    add_population(country, url)
 
+    add_capital(country, doc)
+    add_area(country, doc)
+    add_government(country, doc)
+    add_population(country, doc)
+
+    # handle special cases
+    if president:
+        if president[0] == '\xa0':
+            president = ""
+    if prime_minister:
+        if prime_minister[0].find("TBA") != -1:
+            prime_minister = ""
     add_president_or_prime_minister(country, president, url_president, data_labels[0])
     add_president_or_prime_minister(country, prime_minister, url_prime_minister, data_labels[1])
 
-
 def initialize_crawl():
+    """
+    This function manages the queue of country links.
+    The function sends country link to get_from_url function until the queue is empty
+    Creates the ontology
+    """
     # queue of urls
-    dict = {}
     from_source_url_to_queue()
     while not url_queue.empty():
         job = url_queue.get()
         # print(job)
         get_from_url(job)
-        #print(dict)
-        #print(dict['Xi Jinping'])
-    #print(dict.keys())
     g.serialize("ontology.nt", format="nt")
-
-
 
 # *** Part 2 - Answer Questions ***
 
